27723d6de2bed816b9dc86d1fe1de11e6a887375
MasahiroToyooka <ehara2481@gmail.com>
2020-03-05 03:45:42 +0900

    day08

---
+++ makefile
@@ -0,0 +1,26 @@
+NAME = libf.a
+SRCS = srcs/_putchar.c srcs/_swap.c srcs/_putstr.c srcs/_strlen.c \
+	   srcs/_strcmp.c
+
+OBJS = ${SRCS:.c=.o}
+
+CC		= gcc
+RM		= rm -f
+
+CFLAGS = -Wall -Wextra -Werror
+
+.c.o:
+		${CC} ${CFLAGS} -I includes -c $< -o ${<:.c=.o}
+
+$(NAME): ${OBJS}
+		ar rcs ${NAME} ${OBJS}
+
+all:	${NAME}
+
+clean:
+		${RM} ${OBJS}
+
+fclean:	clean
+		${RM} ${NAME}
+
+re:		fclean all
---
+++ split.c
@@ -0,0 +1,101 @@
+#include <stdlib.h>
+
+char	*_strncpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int		i;
+
+	i = 0;
+	while (src[i] && i < n)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+
+int		_is_separator(char c, char *charset)
+{
+	int		i;
+
+	i = 0;
+	while (charset[i])
+	{
+		if (c == charset[i])
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+char	*_get_next_str(char **pos_in_str, char *charset, int *next_str_len)
+{
+	int		i;
+	char	*str_start;
+
+	*next_str_len = 0;
+	str_start = 0;
+	i = 0;
+	while ((*pos_in_str)[i])
+	{
+		if (_is_separator((*pos_in_str)[i], charset) && str_start != 0)
+		{
+			*pos_in_str = str_start + *next_str_len;
+			return (str_start);
+		}
+		else if (!_is_separator((*pos_in_str)[i], charset) && str_start == 0)
+			str_start = &(*pos_in_str)[i];
+		if (!_is_separator((*pos_in_str)[i], charset))
+			*next_str_len = *next_str_len + 1;
+		i++;
+	}
+	*pos_in_str = str_start + *next_str_len;
+	if (*next_str_len == 0)
+		return (0);
+	return (str_start);
+}
+
+char	**_build_tab(char *str, char *charset)
+{
+	int		nb_str;
+	char	**strs;
+	int		next_str_len;
+	char	*pos_in_str;
+
+	nb_str = 0;
+	next_str_len = 0;
+	pos_in_str = str;
+	while (_get_next_str(&pos_in_str, charset, &next_str_len))
+		nb_str++;
+	if (!(strs = (char **)malloc(sizeof(char *) * (nb_str + 1))))
+		return (0);
+	return (strs);
+}
+
+char	**_split(char *str, char *charset)
+{
+	char	**strs;
+	int		next_str_len;
+	char	*next_str;
+	char	*pos_in_str;
+	int		i;
+
+	if (!(strs = _build_tab(str, charset)))
+		return (0);
+	i = 0;
+	pos_in_str = str;
+	while ((next_str = _get_next_str(&pos_in_str, charset, &next_str_len)))
+	{
+		if (!(strs[i] = (char *)malloc(sizeof(char) * next_str_len + 1)))
+			return (0);
+		_strncpy(strs[i], next_str, next_str_len);
+		i++;
+	}
+	strs[i] = 0;
+	return (strs);
+}
